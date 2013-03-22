65857b66aabca2b553efd4982e4df03e743f695a
gpiazzese <gpiazzese@beintoo.com>
2013-03-22 12:32:57 +0100

    The Sdk now support Russian language, fixed offset of Reward and Give Bedollars when the status bar is not hidden, fixed a minor bug when the internet connection was not available

--- Classes/BeintooAppDelegate.m
+++ Classes/BeintooAppDelegate.m
@@ -66,8 +66,7 @@
 
 	sampleDelegate = [[BeintooDelegate alloc] init];
 	
-    [Beintoo initWithApiKey:@"DuQEh4lGtMGe" andApiSecret:nil andBeintooSettings:beintooSettings andMainDelegate:sampleDelegate];
-    //[Beintoo _privateSandbox];
+    [Beintoo initWithApiKey:your_apikey_here andApiSecret:nil andBeintooSettings:beintooSettings andMainDelegate:sampleDelegate];
     
     /*  UNCOMMENT the line below to use Beintoo in our testing environment sandbox 
 	*   
